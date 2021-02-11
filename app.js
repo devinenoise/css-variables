43df19878675d317867e87f5a359ab1d3fd2b393
Kyle Devine <devinenoise@gmail.com>
2021-02-10 18:34:43 -0800

    border blur and color selectors

---
+++ app.js
@@ -0,0 +1,14 @@
+const inputs = document.querySelectorAll('.controls input');
+
+console.log(inputs);
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || ''; //  or '' allows for the hex code color picker
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix // append the suffix to the element
+  );
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
